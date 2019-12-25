c00f5158b93cf211b4fe651a7bdd8ca33b8771e2
test <pawelkluk12@interia.pl>
2019-12-25 03:38:38 +0100

    Updates

---
+++ precache-manifest.76b798ec1453353ae3f1eaff26df7c9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "213249d59d2195bac0251bea5b37bd9d",
+    "url": "/zwiedzRzeszow/index.html"
+  },
+  {
+    "revision": "6506c5ec8112c796620d",
+    "url": "/zwiedzRzeszow/static/css/main.b26a52e9.chunk.css"
+  },
+  {
+    "revision": "2e869df27a00c3ef4287",
+    "url": "/zwiedzRzeszow/static/js/2.4f2bc4f3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/zwiedzRzeszow/static/js/2.4f2bc4f3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6506c5ec8112c796620d",
+    "url": "/zwiedzRzeszow/static/js/main.a1a5bbaa.chunk.js"
+  },
+  {
+    "revision": "de4a25bf17c803a99f52",
+    "url": "/zwiedzRzeszow/static/js/runtime-main.a74c04f2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/zwiedzRzeszow/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e2d79aeebaed6820599cef71269453cd.js"
+  "/zwiedzRzeszow/precache-manifest.76b798ec1453353ae3f1eaff26df7c9a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/zwiedzRzeszow/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
